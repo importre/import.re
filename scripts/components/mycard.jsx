fa2a4405e16677c522ab0d3acfa971dc984fd587
Jaewe Heo <jaeweheo@gmail.com>
2015-07-01 21:17:52 +0900

    update

--- scripts/components/mycard.jsx
+++ scripts/components/mycard.jsx
@@ -2,31 +2,11 @@ import React from 'react';
 import mui from 'material-ui';
 import bs from 'react-bootstrap';
 
-const {
-  Card,
-  CardMedia,
-  CardTitle,
-  List,
-  ListDivider,
-  ListItem,
-} = mui;
-
-const {
-  Row,
-  Col,
-} = bs;
-
+const { Card, CardMedia, CardTitle, FontIcon, List, ListDivider, ListItem, SvgIcon, } = mui;
+const { Row, Col, } = bs;
 const ThemeManager = new mui.Styles.ThemeManager();
 
 let MyCard = React.createClass({
-
-  getInitialState() {
-    return {
-      name: 'Jaewe Heo',
-      nickname: 'importre',
-      photo: 'images/importre.jpeg'
-    }
-  },
 
   childContextTypes: {
     muiTheme: React.PropTypes.object
@@ -42,20 +22,28 @@ let MyCard = React.createClass({
     return (
       <div className="container">
         <Row className="topbottom">
-          <Col smOffset={3} sm={6}>
+          <Col smOffset={4} sm={4}>
             <Card>
-              <CardMedia overlay={<CardTitle title={this.state.name} subtitle={this.state.nickname}/>}>
-                <img src={this.state.photo}/>
+              <CardMedia
+                overlay={<CardTitle title={"Jaewe Heo"} subtitle={"importre"}/>}>
+                <img src={"images/importre.jpg"}/>
               </CardMedia>
-              <ListDivider/>
-              <List>
-                <ListItem href="http://riiid.co">Riiid!</ListItem>
+              <List subheader="Work">
+                <ListItem leftIcon={<i className="material-icons">work</i>}
+                          secondaryText={"Software Engineer / Co-founder"}
+                          secondaryTextLines={2} href="http://riiid.co">
+                  Riiid!
+                </ListItem>
               </List>
+
               <ListDivider/>
-              <List>
-                <ListItem href="https://github.com/importre">Github</ListItem>
-                <ListItem href="https://facebook.com/importre">Facebook</ListItem>
-                <ListItem href="https://twitter.com/importre">Twitter</ListItem>
+              <List subheader="Social">
+                <ListItem leftIcon={<i className="material-icons">star</i>}
+                          href="https://github.com/importre">Github</ListItem>
+                <ListItem insetChildren={true}
+                          href="https://facebook.com/importre">Facebook</ListItem>
+                <ListItem insetChildren={true}
+                          href="https://twitter.com/importre">Twitter</ListItem>
               </List>
             </Card>
           </Col>
